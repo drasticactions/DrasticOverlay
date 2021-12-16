c45d5f0ed417132bf8b57f91738a167cabbaaf1b
Tim Miller <innerlogic4321@gmail.com>
2021-12-16 11:47:24 -0500

    Update Overlay

--- DrasticOverlay.Sample/TestOverlayPage.xaml.cs
+++ DrasticOverlay.Sample/TestOverlayPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Maui;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Shapes;
 using Microsoft.Maui.Controls.Xaml;
 using System;
 using System.Collections.Generic;
@@ -13,11 +14,23 @@ namespace DrasticOverlay.Sample
     public partial class TestOverlayPage : ContentPage
     {
         TestWindow window;
-
+        
         public TestOverlayPage(TestWindow window)
         {
             this.window = window;
             InitializeComponent();
+            UpdateBorderView();
+        }
+
+        private void UpdateBorderView()
+        {
+            var borderShape = new RoundRectangle
+            {
+                CornerRadius = new CornerRadius(2, 2, 2, 2)
+            };
+
+            BorderView.StrokeShape = borderShape;
+            BorderView.Stroke = new SolidColorBrush(Colors.Black);
         }
 
         private async void OnPageOverlay(object sender, EventArgs e)
